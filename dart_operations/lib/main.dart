1a852cd96f5780a9e380548d918706e3230033b9
Kadriye <kadriyemacit9655@gmail.com>
2022-05-09 20:49:34 +0100

    Add class topic

--- dart_operations/lib/main.dart
+++ dart_operations/lib/main.dart
@@ -1,32 +1,7 @@
-import 'package:dart_operations/homeworks/answers/17_april/homeworks_answers.dart';
-import 'package:dart_operations/homeworks/answers/19_april/homeworks_answers.dart';
-import 'package:dart_operations/homeworks/answers/22_april/homeworks_answers.dart';
-import 'package:dart_operations/homeworks/berkay/16_april/berkay_answers.dart';
-import 'package:dart_operations/homeworks/berkay/16_april/berkay_answers2.dart';
-import 'package:dart_operations/homeworks/berkay/16_april/berkay_answers3.dart';
-import 'package:dart_operations/homeworks/berkay/17_april/answers.dart';
-import 'package:dart_operations/homeworks/berkay/19_april/homework2.dart';
-import 'package:dart_operations/homeworks/berkay/19_april/homework3.dart';
-import 'package:dart_operations/homeworks/berkay/19_april/homework4.dart';
-import 'package:dart_operations/homeworks/berkay/21_april/random_examples.dart';
-import 'package:dart_operations/homeworks/berkay/22_april/homeworks1.dart';
-import 'package:dart_operations/homeworks/berkay/22_april/homeworks2.dart';
-import 'package:dart_operations/homeworks/berkay/22_april/homeworks3.dart';
-import 'package:dart_operations/homeworks/berkay/22_april/homeworks4.dart';
-import 'package:dart_operations/homeworks/berkay/22_april/homeworks5.dart';
 import 'package:dart_operations/homeworks/berkay/24_april/homeworks2.dart';
-import 'package:dart_operations/homeworks/berkay/24_april/homeworks3.dart';
-import 'package:dart_operations/homeworks/berkay/24_april/homeworks4.dart';
-import 'package:dart_operations/homeworks/berkay/24_april/homeworks6.dart';
-import 'package:dart_operations/homeworks/berkay/24_april/homeworks7.dart';
-import 'package:dart_operations/homeworks/berkay/24_april/homeworks8.dart';
-import 'package:dart_operations/homeworks/berkay/24_april/homeworks_answers_berkay.dart';
-import 'package:dart_operations/homeworks/berkay/25_april/homeworks1.dart';
 import 'package:dart_operations/homeworks/berkay/25_april/homeworks2.dart';
-import 'package:dart_operations/lessons/functions.dart';
-import 'package:dart_operations/lessons/list_examples.dart';
-import 'package:dart_operations/lessons/loops/loop_while.dart';
-import 'package:dart_operations/lessons/set_examples.dart';
+import 'package:dart_operations/lessons/class/book_class.dart';
+import 'package:dart_operations/lessons/class/class_examples.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -80,15 +55,14 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              oddNumber().toString(),
+              "oddNumber().toString()",
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () =>
-            letsGo(), // oluşturduğumuz fonksiyon isimlerini buraya yazıyoruz
+        onPressed: () => exampleBooksClass(), // oluşturduğumuz fonksiyon isimlerini buraya yazıyoruz
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
